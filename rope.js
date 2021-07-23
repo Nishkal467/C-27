5a26abc91ef8bd8a9fe6647d26f2035d1048ddf4
Nishkal <72551241+Nishkal467@users.noreply.github.com>
2021-07-23 23:23:53 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,24 @@
+class rope{
+	constructor(body1,body2, offsetX, offsetY)
+	{
+		var options={
+			bodyA:body1,
+			bodyB:body2,
+		}
+
+		this.rope=Constraint.create(options)
+		World.add(world,this.rope)
+
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+	}
+
+	display()
+	{
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
+        fill("red")
+		strokeWeight(2);
+	}
+
+}
